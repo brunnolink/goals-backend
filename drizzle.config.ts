a2dbbf776f508dc97bf4af671cd091893815d1f1
brunno <brunnolinker@gmail.com>
2024-09-10 09:43:54 -0300

    FEAT: projeto iniciado

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import {defineConfig} from 'drizzle-kit';
+import { env } from './src/http/env';
+
+
+export default defineConfig({
+    schema: "./schema.ts",
+    out: './.migrations',
+    dialect: 'postgresql',
+    dbCredentials: {
+        url: env.DATABASE_URL,
+    }
+})
