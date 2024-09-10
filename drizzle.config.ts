a338380033c1150b82c9af96b3ac0d22bd4903ec
brunno <brunnolinker@gmail.com>
2024-09-10 14:40:37 -0300

    feat: db.migrations criada

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { env } from './src/http/env';
 
 
 export default defineConfig({
-    schema: "./schema.ts",
+    schema: "./src/db/schema.ts",
     out: './.migrations',
     dialect: 'postgresql',
     dbCredentials: {
